0db8eddedd090f5a9171c8b7551df89c680a076a
Kiel Howell <kiel.howell@pgi.com>
2020-02-19 18:47:56 -0500

    Add project files.

---
+++ ListifyWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListifyWebAPI.Controllers
+{
+    [Route("listify")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET listify?begin=&end=&idx=
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(int? begin, int? end, int? idx)
+        {
+            if (begin == null || end == null || idx == null)
+            {
+                string httpString = this.Request.IsHttps ? "https://" : "http://";
+                return new string[] { "Enter url as " + httpString + this.Request.Host  + "/listify?begin=x&end=x&idx=x", "replace the 'x' after each '=' with whatever values you want."  };
+            }
+
+            Listify _list = new Listify();
+            _list.AddRange(Enumerable.Range(begin.Value, end.Value));
+
+            return new string[] { _list[idx.Value].ToString() };
+        }
+    }
+}
---
+++ ListifyWebAPI/Listify.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListifyWebAPI
+{
+    class Listify : IList<int>, IEnumerable<int>
+    {
+        private IList<int> _iList = new List<int>();
+
+        public int this[int index]
+        {
+            get
+            {
+                return _iList[index];
+            }
+
+            set
+            {
+                _iList[index] = value;
+            }
+        }
+
+        int IList<int>.this[int index]
+        {
+            get
+            {
+                return _iList[index];
+            }
+            set
+            {
+                _iList[index] = value;
+            }
+        }
+
+        internal void AddRange(IEnumerable<int> enumerable)
+        {
+            foreach (int elem in enumerable)
+            {
+                _iList.Add(elem);
+            }
+        }
+
+        int ICollection<int>.Count => throw new NotImplementedException();
+
+        bool ICollection<int>.IsReadOnly => throw new NotImplementedException();
+
+        void ICollection<int>.Add(int item)
+        {
+            _iList.Add(item);
+        }
+
+        void ICollection<int>.Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICollection<int>.Contains(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<int>.CopyTo(int[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            return _iList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IList<int>.IndexOf(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IList<int>.Insert(int index, int item)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        bool ICollection<int>.Remove(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IList<int>.RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
